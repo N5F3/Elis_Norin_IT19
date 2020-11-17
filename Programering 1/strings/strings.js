62a869f6264c5d4e302fe44be5a06e384b4cd8ab
N5F3 <elis.norin@elev.ga.ntig.se>
2020-11-17 09:36:22 +0100

    okay

--- Programering 1/strings/strings.js
+++ Programering 1/strings/strings.js
@@ -6,7 +6,7 @@ let fornamn = "Elis"
 let efternamn = "Norin"
 let namn = fornamn + " " + efternamn
 let adress = "Kronhusgatan 9"
-let swish_nr = 070761
+let swish_nr =  " 0707618816"
 let alder = 17
 
 console.log("namn: " + namn +"\n" + "adress: " + adress + "\n" + "swish nr:" + swish_nr + "\n" + "alder: " +alder)
@@ -26,10 +26,22 @@ console.log(`Bokstav på index 4 är: ${bokstav5}`)
 let antal_bokstaver = alfabet.length
 console.log(`antal bokstäver i alfabetet är ${antal_bokstaver}`)
 
-//slice
+console.log("Jag Läser IT-Programmet på Nordens Tekniker Institut Kronhus i Göteborg")
 
-//sökning
+let uppgift = "Jag Läser IT-Programmet på Nordens Tekniker Institut Kronhus i Göteborg"
+let bokstavertotalt = uppgift.split(/ |-/gm);
+console.log(`antal ord: ${bokstavertotalt.length}`)
 
-//split
+let vokaler = /a|e|i|o|u|y|å|ä|ö/gm
+let vokalord = "LatjoLajban";
+console.log(`Antal vokaler i ordet ${vokalord} är: ${vokalord.match(vokaler).length}`)
 
-//loopa över en lista
+
+let mening ="Jag Läser IT-Programmet på Nordens Tekniker Institut Kronhus i Göteborg"
+let ord = mening.split(" ")
+
+console.log(ord)
+console.log(ord[2])
+
+let antal_ord = ord.length
+console.log(`Antal ord i meningen är ${antal_ord}`)
